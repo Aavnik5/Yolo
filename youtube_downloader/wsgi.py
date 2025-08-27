7e55659f3fd6399baf92113b4b700c6a6cd3639b
Aavnik5 <aav5roy@gmail.com>
2025-08-27 17:41:26 +0530

    message

--- youtube_downloader/wsgi.py
+++ youtube_downloader/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
+
 import os
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_downloader.settings')
 
 application = get_wsgi_application()
+gunicorn yolo1.wsgi:application
